0b743922e82efa61b2d375ef61df5da7f191b242
nurzainpradana <pradananurzain@gmail.com>
2021-01-10 20:40:04 +0700

    initial commit

---
+++ app/src/main/java/com/zainpradana/belajarkotlin/filmapp/SplashScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.zainpradana.belajarkotlin.filmapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
