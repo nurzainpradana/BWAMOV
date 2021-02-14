02634c297cda6d172ba8f42d8cc1ed070c86a1f7
nurzainpradana <pradananurzain@gmail.com>
2021-02-14 13:46:20 +0700

    create on boarding one xml

---
+++ app/src/main/java/com/zainpradana/belajarkotlin/filmapp/onboard/OnBoardingOneActivity.kt
@@ -0,0 +1,12 @@
+package com.zainpradana.belajarkotlin.filmapp.onboard
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zainpradana.belajarkotlin.filmapp.R
+
+class OnBoardingOneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding_one)
+    }
+}
